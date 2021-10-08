bb1ee3e2c2f9a813b11c281e87c0a900e4206165
Admin <Admin@DESKTOP-UG0GOA7>
2021-10-08 13:41:49 +0100

    Kata2 versión 2

--- src/main/java/kata2_rgs/kata2_rgs.java
+++ src/main/java/kata2_rgs/kata2_rgs.java
@@ -18,13 +18,17 @@ public class kata2_rgs {
             data[i]= i;
         }
         
-        for (int i = 0; i < data.length; i++) {
+        /*for (int i = 0; i < data.length; i++) {
             if (histogram.containsKey(data[i])){
                 histogram.put(data[i], histogram.get(data[i])+1);
             }else {
                 histogram.put(data[i], 1);
             }
             
+        }*/
+        
+        for (int key : data) {
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
         }
         
         for (Integer key : histogram.keySet()) {
